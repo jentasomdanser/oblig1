b751aca295edc20dea74901fa1b76becbe354644
Emma Agnethe <142502961+jentasomdanser@users.noreply.github.com>
2024-02-14 15:23:43 +0100

    Begynte å skrive inn html og javascript

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,15 @@
+function kjopBilletter(){
+    const film = document.getElementById("Film");
+    const antallBilletter = document.getElementById("antall");
+    const forNavn = document.getElementById("forNavn");
+    
+
+}
+
+function slettBillettene(){
+    const order = {
+        film,
+        
+
+    }
+}
