a5b14c4d2e02a6cda566a95a32de749f3760d242
Emma Agnethe <142502961+jentasomdanser@users.noreply.github.com>
2024-02-15 12:54:19 +0100

    Mer skriving i html og script

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,6 +2,10 @@ function kjopBilletter(){
     const film = document.getElementById("Film");
     const antallBilletter = document.getElementById("antall");
     const forNavn = document.getElementById("forNavn");
+    const etterNavn = document.getElementById("etteNavn");
+    const phoneNumber = document.getElementById("phoneNumber");
+    const epost = document.getElementById("email");
+
     
 
 }
